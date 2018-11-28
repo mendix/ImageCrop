dbdc5d5ace923ea5ad8c51f9e656930dc7b1f42b
Osama Najjar <osama.najjar@mendix.com>
2018-11-28 17:01:24 +0100

    next working draft with rotation enabled

--- src/ImageCrop/widget/ImageCrop.js
+++ src/ImageCrop/widget/ImageCrop.js
@@ -70,6 +70,8 @@ export default defineWidget('ImageCrop', template, {
                 this.crop.bind({
                     url: this.imageSrc,
                 });
+
+                this.croppingWrapper.addEventListener("update", hitch(this, this._resetCropperOffsets));
                 this._createControls();
             }
 
@@ -95,19 +97,22 @@ export default defineWidget('ImageCrop', template, {
         // set boundaries only if they are not zeros, otherwise it will be default to the container size.
         if (this.boundaryWidth !== 0 && this.boundaryHeight !== 0) {
             options.boundary = {};
-            this.boundaryWidth ? options.boundary.width = this.boundaryWidth : false;
-            this.boundaryHeight ? options.boundary.height = this.boundaryHeight : false;
+            this.boundaryWidth && (options.boundary.width = this.boundaryWidth);
+            this.boundaryWidth && (options.boundary.width = this.boundaryWidth);
+            this.boundaryHeight && (options.boundary.height = this.boundaryHeight);
+
         }
 
         // toggle zoomer, resize, and orientation
         this.enableZoomer && (options.enableZoomer = this.enableZoomer);
         this.enableResize && (options.enableResize = this.enableResize);
-        this.enableOrientation && (options.enableOrientation = this.enableOrientation);
+        options.enableOrientation = true; // required for rotation functionality.
 
         // set mouse wheel zoom option
         this.mouseWheelZoom === 'yes' && (options.mouseWheelZoom = true);
         this.mouseWheelZoom === 'yesWithCtrl' && (options.mouseWheelZoom = 'ctrl');
         this.mouseWheelZoom === 'no' && (options.mouseWheelZoom = false);
+
 
         console.log(options);
         return options;
@@ -118,51 +123,81 @@ export default defineWidget('ImageCrop', template, {
         const croppedImageBlob = await this.crop.result('blob');
         console.log(croppedImageBlob);
     },
+    _rotateLeft() {
+        this.log('_rotateLeft');
+        this.crop.rotate(90);
+
+    },
+    _rotateRight() {
+        this.log('_rotateRight');
+        this.crop.rotate(-90);
+    },
 
     _createControls() {
         this.log('_createControls');
+        this.controlsWrapper = domCreate("div", {
+            class: `btn-group image-crop-controls`,
+        });
+
         this.cropBtn = domCreate("button", {
-            class: `btn btn-${this.cropButtonStyle} image-crop-controls__crop-btn`,
+            class: `btn btn-${this.cropButtonStyle} btn-${this.cropButtonPosition} image-crop-btn image-crop-btn--crop`,
             innerHTML: "Crop"
         });
+        this.rotateLeftBtn = domCreate("button", {
+            class: `btn btn-${this.cropButtonStyle} btn-${this.cropButtonPosition} image-crop-btn image-crop-btn--rotate-l`,
+            innerHTML: "Rotate Left"
+        });
+        this.rotateRightBtn = domCreate("button", {
+            class: `btn btn-${this.cropButtonStyle} btn-${this.cropButtonPosition} image-crop-btn image-crop-btn--rotate-r`,
+            innerHTML: "Rotate Right"
+        });
+        if (this.cropWidgetWrapperBottomMargin) {
+            domStyleSet(this.cropBtn, "margin-bottom", `${this.cropWidgetWrapperBottomMargin}px`);
+            domStyleSet(this.rotateLeftBtn, "margin-bottom", `${this.cropWidgetWrapperBottomMargin}px`);
+            domStyleSet(this.rotateRightBtn, "margin-bottom", `${this.cropWidgetWrapperBottomMargin}px`);
+        }
+        domPlace(this.rotateLeftBtn, this.controlsWrapper);
         domPlace(this.cropBtn, this.controlsWrapper);
-        const cropEventHandler = on(this.cropBtn, "click", hitch(this, this._cropImage));
-        this.eventHandlers.push(cropEventHandler);
+        domPlace(this.rotateRightBtn, this.controlsWrapper);
+        domPlace(this.controlsWrapper, this.croppingWrapper);
 
-        this._setControlOffset();
+
+        const cropEventHandler = on(this.cropBtn, "click", hitch(this, this._cropImage));
+        const rotateLeftEventHandler = on(this.rotateLeftBtn, "click", hitch(this, this._rotateLeft));
+        const rotateRightEventHandler = on(this.rotateRightBtn, "click", hitch(this, this._rotateRight));
+        this.eventHandlers.push(cropEventHandler);
+        this.eventHandlers.push(rotateLeftEventHandler);
+        this.eventHandlers.push(rotateRightEventHandler);
+
+        this._resetCropperOffsets();
 
     },
 
-    _setControlOffset() {
-        this.log('_setControlOffset');
+    _resetCropperOffsets() {
+        this.log('_resetCropperOffsets');
         /**
          * Croppie library doesn't come with a control bar to perform image transfomration
          * so this function makes sure that the control bar that we made is positioned properly 
-         * in the widget.
+         * in the widget along with the zoomer.
          * 
-         * Important Note : should be invoked after initializing croppie and appending controls.
+         * Important Note : should be invoked after initializing croppie.
          */
         if (!this.readOnly) {
-            // get crop boundary height
+
             const cropBoundaryNode = domQuery(".cr-boundary", this.domNode)[0] || null;
-            const cropBoundaryHeight = cropBoundaryNode ? (parseInt(cropBoundaryNode.style.height || cropBoundaryNode.offsetHeight, 10)) : 0;
+            const cropBoundaryWidth = cropBoundaryNode ? (parseInt(cropBoundaryNode.style.width || cropBoundaryNode.offsetWidth, 10)) : 0;
 
-            const zoomSliderWrapper = domQuery(".cr-slider-wrap", this.domNode)[0] || null;
-            const zoomSliderWrapperHeight = zoomSliderWrapper ? (parseInt(zoomSliderWrapper.style.height || zoomSliderWrapper.offsetHeight, 10)) : 0;
 
-            const controlsWrapperHeight = this.cropBtn ? (parseInt(this.controlsWrapper.style.height || this.controlsWrapper.offsetHeight, 10)) : 0;
+            const cropSliderWrapper = domQuery(".cr-slider-wrap", this.domNode)[0] || null;
 
-            console.log(cropBoundaryHeight, zoomSliderWrapperHeight, controlsWrapperHeight);
-            // make sure not to hide the widget.
-            (cropBoundaryHeight + zoomSliderWrapperHeight + controlsWrapperHeight) && domStyleSet(this.domNode, "height", `${(cropBoundaryHeight + zoomSliderWrapperHeight + controlsWrapperHeight)}px`);
-            (cropBoundaryHeight + zoomSliderWrapperHeight + controlsWrapperHeight) && domStyleSet(this.domNode, "margin-bottom", `${controlsWrapperHeight + this.cropWidgetWrapperBottomMargin}px`);
-
+            this.enableZoomer && (domStyleSet(cropSliderWrapper, "width", `${cropBoundaryWidth}px`));
+            this.boundaryWidth && (domStyleSet(this.controlsWrapper, "width", `${cropBoundaryWidth}px`));
         }
 
     },
 
     resize() {
         this.log('resize');
-        this._setControlOffset();
+        this._resetCropperOffsets();
     }
 });
