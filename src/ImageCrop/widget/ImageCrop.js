84c1b87fa21d4c195caf2066a828e7a5b990133e
Osama Najjar <osama.najjar@mendix.com>
2018-11-29 13:26:11 +0100

    new 90% done draft

--- src/ImageCrop/widget/ImageCrop.js
+++ src/ImageCrop/widget/ImageCrop.js
@@ -2,6 +2,7 @@ import {
     defineWidget,
     log,
     runCallback,
+    execute as executeMF
 } from 'widget-base-helpers';
 import {
     set as domAttrSet,
@@ -13,7 +14,8 @@ import {
 } from "dojo/dom-style";
 import {
     create as domCreate,
-    place as domPlace
+    place as domPlace,
+    empty as domEmpty
 } from "dojo/dom-construct";
 
 import {
@@ -23,14 +25,16 @@ import on from "dojo/on";
 import domQuery from "dojo/query";
 import {
     isMxImageObject
-} from "./MxDataUtils";
+} from "./utils/MxDataUtils";
 import {
     setErrorFeedbackNode
-} from "./WidgetDomUtils";
+} from "./utils/WidgetDomUtils";
 
 import {
     Croppie
 } from 'croppie/croppie';
+
+
 
 
 
@@ -45,6 +49,7 @@ export default defineWidget('ImageCrop', template, {
         this.log = log.bind(this);
         this.runCallback = runCallback.bind(this);
         this.eventHandlers = [];
+        this.nextRotate = 0;
 
     },
 
@@ -53,6 +58,65 @@ export default defineWidget('ImageCrop', template, {
         this.imageSrc = "";
 
     },
+
+    _initCropedImageObject(MxEntityName, callback) {
+        this.log('postCreate');
+        mx.data.create({
+            entity: MxEntityName,
+            callback: hitch(this, (object) => {
+                this.croppedImageMxObject = object;
+                this.runCallback(callback, "return control to widget base");
+            }),
+            error: hitch(this, (error) => {
+                setErrorFeedbackNode(`Oops, Something went wrong while initialzing the data structure required to receive the resulted image.`, this.domNode, this.domNode);
+                console.error(error.message, error);
+                this.runCallback(callback, "return control to widget base.");
+            })
+        });
+    },
+
+    _saveResultedImage(blob) {
+        this.log('_saveResultedImage');
+        if (mx.data.saveDocument) {
+            mx.data.saveDocument(this.croppedImageMxObject.getGuid(), `${this.contextObject.get("Name")}_cropped${new Date().getUTCDate()}`, {
+                width: 180,
+                height: 180
+            }, blob, hitch(this, this._returnToUserHandling), error => {
+                setErrorFeedbackNode("Oops! Something went wrong, please check your cosole for more info about the erro.");
+                console.error(error.message, error);
+            });
+        } else if (mendix.lib.Upload) {
+            const Uploader = mendix.lib.Upload;
+            const croppedImageUploader = new Uploader({
+                objectGuid: this.croppedImageMxObject.getGuid(),
+                maxFileSize: blob.size,
+                startUpload: null,
+                finishUpload: null,
+                form: {
+                    mxdocument: {
+                        files: [
+                            blob
+                        ]
+                    }
+                },
+                callback: hitch(this, this._returnToUserHandling),
+                error: hitch(this, (error) => {
+                    setErrorFeedbackNode("Oops! Something went wrong, please check your cosole for more info about the erro.");
+                    console.error(error.message, error);
+                })
+            });
+            croppedImageUploader.upload();
+        }
+    },
+
+
+    _returnToUserHandling() {
+        this.log("_returnToUserHandling");
+        executeMF.call(this, this.cropedImageMicroflowHandler, this.croppedImageMxObject.getGuid(), () => {
+            this.log("successfully triggered Microflow")
+        });
+    },
+
 
     postCreate() {
         this.log('postCreate');
@@ -71,14 +135,20 @@ export default defineWidget('ImageCrop', template, {
                     url: this.imageSrc,
                 });
 
-                this.croppingWrapper.addEventListener("update", hitch(this, this._resetCropperOffsets));
+                this.croppingWrapper.addEventListener("update", hitch(this, (cropUpdateEvent) => {
+                    this._resetCropperOffsets();
+                    this._cropImageForView();
+                }));
                 this._createControls();
+                this._initCropedImageObject(object.getEntity(), callback);
+
             }
 
         } else {
             setErrorFeedbackNode(`Please place this widget in a context that inherits from 'System.Image' entity.`, this.domNode, this.domNode);
+            this.runCallback(callback, "update");
         }
-        this.runCallback(callback, "update");
+
     },
 
 
@@ -118,25 +188,56 @@ export default defineWidget('ImageCrop', template, {
         return options;
     },
 
-    async _cropImage() {
+    async _cropImageForSave() {
         this.log('_cropImage');
-        const croppedImageBlob = await this.crop.result('blob');
+        const croppedImageBlob = await this.crop.result(this._getCroppedImageOptions('blob'));
+        this._saveResultedImage(croppedImageBlob);
         console.log(croppedImageBlob);
+    },
+    async _cropImageForView() {
+        this.log('_cropImage');
+        const croppedImageHTML = await this.crop.result(this._getCroppedImageOptions('html'));
+        domStyleSet(this.croppedImageViewWrapper, "display", "block");
+        domEmpty(this.croppedImageViewWrapper);
+        domPlace(croppedImageHTML, this.croppedImageViewWrapper);
+        /*if (!this.croppedImageMxObject) {
+
+        }*/
+    },
+
+    _getCroppedImageOptions(type) {
+        this.log('_getCroppedImageOptions');
+        const quality = (isNaN(parseFloat(this.croppedImageQuality)) && parseFloat(this.croppedImageQuality) >= 0 && parseFloat(this.croppedImageQuality) <= 1) ? parseFloat(this.croppedImageQuality) : 1;
+        return {
+            type: type,
+            format: this.croppedImageFormat,
+            quality: quality,
+            size: this.croppedImageSize,
+            circle: this.croppedImageIsCircle
+        };
     },
     _rotateLeft() {
         this.log('_rotateLeft');
         this.crop.rotate(90);
+        this.nextRotate === 360 && (this.nextRotate = 0); // reset
+        this.nextRotate += 90;
+
 
     },
     _rotateRight() {
         this.log('_rotateRight');
         this.crop.rotate(-90);
+        this.nextRotate === -360 && (this.nextRotate = 0); // reset
+        this.nextRotate -= 90;
     },
 
     _createControls() {
         this.log('_createControls');
         this.controlsWrapper = domCreate("div", {
             class: `btn-group image-crop-controls`,
+        });
+        this.croppedImageViewWrapper = domCreate("div", {
+            class: `image-crop-cropped-image-viewer`,
         });
 
         this.cropBtn = domCreate("button", {
@@ -155,14 +256,16 @@ export default defineWidget('ImageCrop', template, {
             domStyleSet(this.cropBtn, "margin-bottom", `${this.cropWidgetWrapperBottomMargin}px`);
             domStyleSet(this.rotateLeftBtn, "margin-bottom", `${this.cropWidgetWrapperBottomMargin}px`);
             domStyleSet(this.rotateRightBtn, "margin-bottom", `${this.cropWidgetWrapperBottomMargin}px`);
+            domStyleSet(this.croppedImageViewWrapper, "margin-bottom", `${this.cropWidgetWrapperBottomMargin}px`);
         }
         domPlace(this.rotateLeftBtn, this.controlsWrapper);
         domPlace(this.cropBtn, this.controlsWrapper);
         domPlace(this.rotateRightBtn, this.controlsWrapper);
         domPlace(this.controlsWrapper, this.croppingWrapper);
+        domPlace(this.croppedImageViewWrapper, this.croppingWrapper);
 
 
-        const cropEventHandler = on(this.cropBtn, "click", hitch(this, this._cropImage));
+        const cropEventHandler = on(this.cropBtn, "click", hitch(this, this._cropImageForSave));
         const rotateLeftEventHandler = on(this.rotateLeftBtn, "click", hitch(this, this._rotateLeft));
         const rotateRightEventHandler = on(this.rotateRightBtn, "click", hitch(this, this._rotateRight));
         this.eventHandlers.push(cropEventHandler);
@@ -191,7 +294,8 @@ export default defineWidget('ImageCrop', template, {
             const cropSliderWrapper = domQuery(".cr-slider-wrap", this.domNode)[0] || null;
 
             this.enableZoomer && (domStyleSet(cropSliderWrapper, "width", `${cropBoundaryWidth}px`));
-            this.boundaryWidth && (domStyleSet(this.controlsWrapper, "width", `${cropBoundaryWidth}px`));
+            cropBoundaryWidth && (domStyleSet(this.controlsWrapper, "width", `${cropBoundaryWidth}px`));
+            cropBoundaryWidth && (domStyleSet(this.croppedImageViewWrapper, "width", `${cropBoundaryWidth}px`));
         }
 
     },
